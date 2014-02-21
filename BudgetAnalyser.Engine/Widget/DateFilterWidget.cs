88914594710511a3dc1bce9e68133f5ebdfb83b0
Benrnz <Ben@Rees.biz>
2014-02-22 11:13:56 +1300

    Added second image to Date Filter Widget

--- BudgetAnalyser.Engine/Widget/DateFilterWidget.cs
+++ BudgetAnalyser.Engine/Widget/DateFilterWidget.cs
@@ -4,14 +4,28 @@ namespace BudgetAnalyser.Engine.Widget
 {
     public class DateFilterWidget : Widget
     {
+        private string doNotUseImageResourceName2;
+
         public DateFilterWidget()
         {
             Category = "Global Filter";
             Dependencies = new[] { typeof(GlobalFilterCriteria) };
             ImageResourceName = "DateFilterBeakerImage";
+            ImageResourceName2 = "DateFilterCalendarImage";
             Size = WidgetSize.Medium;
             WidgetStyle = "ModernTileMediumStyle1";
             Clickable = true;
+        }
+
+        // This is a little hacky.  Need to figure out how to combine the ImageSource for both images into one.
+        public string ImageResourceName2
+        {
+            get { return this.doNotUseImageResourceName2; }
+            set
+            {
+                this.doNotUseImageResourceName2 = value;
+                OnPropertyChanged();
+            }
         }
 
         public override void Update(params object[] input)
