c7daf27b2736d0caef657179e84472f970301426
Benrnz <Ben@Rees.biz>
2014-03-22 18:39:17 +1300

    Fixed progress bars on dashboard not updating

--- BudgetAnalyser.Engine/Widget/ProgressBarWidget.cs
+++ BudgetAnalyser.Engine/Widget/ProgressBarWidget.cs
@@ -2,14 +2,43 @@
 {
     public abstract class ProgressBarWidget : Widget
     {
-        public double Maximum { get; set; }
-        public double Minimum { get; set; }
+        private double doNotUseMaximum;
+        private double doNotUseMinimum;
+        private double doNotUseValue;
+
+        public double Maximum
+        {
+            get { return this.doNotUseMaximum; }
+            set
+            {
+                this.doNotUseMaximum = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public double Minimum
+        {
+            get { return this.doNotUseMinimum; }
+            set
+            {
+                this.doNotUseMinimum = value;
+                OnPropertyChanged();
+            }
+        }
 
         public bool ProgressBarVisibility
         {
             get { return true; }
         }
 
-        public double Value { get; set; }
+        public double Value
+        {
+            get { return this.doNotUseValue; }
+            set
+            {
+                this.doNotUseValue = value;
+                OnPropertyChanged();
+            }
+        }
     }
 }
