1bf935c1114f45f894e21c03b04ef91922ddbda0
benrnz <ben@rees.biz>
2014-04-17 17:12:59 +1200

    New grouped container style for Matching rules maintenance

--- BudgetAnalyser/Matching/RulesGroupedByBucket.cs
+++ BudgetAnalyser/Matching/RulesGroupedByBucket.cs
@@ -2,14 +2,18 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using BudgetAnalyser.Engine.Annotations;
 using BudgetAnalyser.Engine.Budget;
 using BudgetAnalyser.Engine.Matching;
 
 namespace BudgetAnalyser.Matching
 {
-    public class RulesGroupedByBucket
+    public class RulesGroupedByBucket : INotifyPropertyChanged
     {
+        private BudgetBucket bucket;
+        private BindingList<MatchingRule> rules;
+
         public RulesGroupedByBucket([NotNull] BudgetBucket bucket, [NotNull] IEnumerable<MatchingRule> rules)
         {
             if (bucket == null)
@@ -26,8 +30,41 @@ namespace BudgetAnalyser.Matching
             Rules = new BindingList<MatchingRule>(rules.ToList());
         }
 
-        public BudgetBucket Bucket { get; private set; }
+        public BudgetBucket Bucket
+        {
+            get { return this.bucket; }
+            private set
+            {
+                this.bucket = value;
+                OnPropertyChanged();
+            }
+        }
 
-        public BindingList<MatchingRule> Rules { get; private set; }
+        public BindingList<MatchingRule> Rules
+        {
+            get { return this.rules; }
+            private set
+            {
+                this.rules = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public int RulesCount
+        {
+            get { return Rules.Count(); }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
     }
 }
