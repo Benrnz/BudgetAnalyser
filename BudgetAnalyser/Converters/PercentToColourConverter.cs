a1495bbc97c784a3140d126623ab613e46f55176
benrnz <ben@rees.biz>
2014-04-27 09:20:19 +1200

    Rename all oil light styles to be prefixed with Badge.
    Rename all converters to be prefixed with Converter.
    Added group totals to group by bucket sort in transactions view. More work to do here.

---
+++ BudgetAnalyser/Converters/PercentToColourConverter.cs
@@ -0,0 +1,102 @@
+using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace BudgetAnalyser.Converters
+{
+    public class PercentToColourConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                var percent = (double)value;
+                if (parameter == null || string.IsNullOrWhiteSpace(parameter.ToString()) || parameter.ToString() == "Expense")
+                {
+                    return ConvertToExpenseColors(percent);
+                }
+
+                if (parameter.ToString() == "Income")
+                {
+                    return ConvertToIncomeColors(percent);
+                }
+
+                if (parameter.ToString() == "Performance")
+                {
+                    return ConvertToPerformanceColors(percent);
+                }
+
+                return ConverterHelper.NeutralNumberBackgroundBrush;
+            }
+            catch (InvalidCastException)
+            {
+                return ConverterHelper.TransparentBrush;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+
+        private static object ConvertToExpenseColors(double percent)
+        {
+            if (percent < 0.67)
+            {
+                return ConverterHelper.CreditBackground1Brush;
+            }
+
+            if (percent < 0.85)
+            {
+                return ConverterHelper.SlightDebitBrush;
+            }
+
+            if (percent < 1.1)
+            {
+                return ConverterHelper.NotSoBadDebitBrush;
+            }
+
+            return ConverterHelper.DebitBackground1Brush;
+        }
+
+        private static object ConvertToIncomeColors(double percent)
+        {
+            if (percent < 0.67)
+            {
+                return ConverterHelper.DebitBackground1Brush;
+            }
+
+            if (percent < 0.85)
+            {
+                return ConverterHelper.NotSoBadDebitBrush;
+            }
+
+            if (percent < 1.00)
+            {
+                return ConverterHelper.SlightDebitBrush;
+            }
+
+            return ConverterHelper.CreditBackground1Brush;
+        }
+
+        private static object ConvertToPerformanceColors(double percent)
+        {
+            if (percent < -100)
+            {
+                return ConverterHelper.DebitBackground1Brush;
+            }
+
+            if (percent < 0)
+            {
+                return ConverterHelper.NotSoBadDebitBrush;
+            }
+
+            if (percent < 100.00)
+            {
+                return ConverterHelper.NeutralNumberBackgroundBrush;
+            }
+
+            return ConverterHelper.CreditBackground1Brush;
+        }
+    }
+}
