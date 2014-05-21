ba2102d958a6de66894dc673290b61bca9b1e7c6
Benrnz <Ben@Rees.biz>
2014-05-21 19:07:45 +1200

    Increment version to 1.7 for June release

--- BudgetAnalyser.Engine/Properties/AssemblyInfo.cs
+++ BudgetAnalyser.Engine/Properties/AssemblyInfo.cs
@@ -37,7 +37,7 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.6.0")]
-[assembly: AssemblyFileVersion("1.6.0")]
+[assembly: AssemblyVersion("1.7.0")]
+[assembly: AssemblyFileVersion("1.7.0")]
 [assembly: InternalsVisibleTo("BudgetAnalyser.UnitTest")]
 [assembly: CLSCompliant(false)]
