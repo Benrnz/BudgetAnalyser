995e621ad3a74f3d0f9eebe15a67883cb9133746
Benrnz <Ben@Rees.biz>
2014-05-25 18:48:16 +1200

    Added a consistency check to ledger book reconciliation.  This is to make sure older locked entries do not change.

--- BudgetAnalyser.Engine/Ledger/LedgerBook.cs
+++ BudgetAnalyser.Engine/Ledger/LedgerBook.cs
@@ -96,6 +96,7 @@ namespace BudgetAnalyser.Engine.Ledger
                 }
             }
 
+            decimal consistencyCheck1 = DatedEntries.Sum(e => e.CalculatedSurplus);
             var newLine = new LedgerEntryLine(date, bankBalances);
             var previousEntries = new Dictionary<LedgerColumn, LedgerEntry>();
             LedgerEntryLine previousLine = this.datedEntries.FirstOrDefault();
@@ -111,6 +112,12 @@ namespace BudgetAnalyser.Engine.Ledger
             }
 
             newLine.AddNew(previousEntries, budget, statement, CalculateStartDateForReconcile(date));
+            decimal consistencyCheck2 = DatedEntries.Sum(e => e.CalculatedSurplus);
+            if (consistencyCheck1 != consistencyCheck2)
+            {
+                throw new CorruptedLedgerBookException("Code Error: The previous dated entries have changed, this is not allowed. Data is corrupt.");
+            }
+
             this.datedEntries.Insert(0, newLine);
             this.newlyAddedLedgers.Clear();
             return newLine;
