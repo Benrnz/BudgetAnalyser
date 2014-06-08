3f8ae9d38931f65bc607345a7f4f668f4be913ba
Benrnz <Ben@Rees.biz>
2014-06-08 12:04:38 +1200

    Moved filters to main toolbar

---
+++ BudgetAnalyser.Engine/Budget/ExpenseBucket.cs
@@ -0,0 +1,15 @@
+﻿namespace BudgetAnalyser.Engine.Budget
+{
+    public abstract class ExpenseBucket : BudgetBucket
+    {
+        protected ExpenseBucket()
+        {
+            // Default constructor required for deserialisation.
+        }
+
+        protected ExpenseBucket(string code, string name)
+            : base(code, name)
+        {
+        }
+    }
+}
