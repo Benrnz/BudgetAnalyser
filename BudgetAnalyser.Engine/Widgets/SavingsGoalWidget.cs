4e5e2d449cc8ec4f6662c9b5dcfbc79e11eb2d3e
Benrnz <Ben@Rees.biz>
2014-06-21 19:06:14 +1200

    Improvements to savings goal widget. Complications with tracked savings legders seeing as both credit and debit transactions are present in the statement.

--- BudgetAnalyser.Engine/Widgets/SavingsGoalWidget.cs
+++ BudgetAnalyser.Engine/Widgets/SavingsGoalWidget.cs
@@ -3,6 +3,7 @@ using System.Globalization;
 using System.Linq;
 using BudgetAnalyser.Engine.Annotations;
 using BudgetAnalyser.Engine.Budget;
+using BudgetAnalyser.Engine.Ledger;
 using BudgetAnalyser.Engine.Statement;
 
 namespace BudgetAnalyser.Engine.Widgets
@@ -14,7 +15,7 @@ namespace BudgetAnalyser.Engine.Widgets
         public SavingsGoalWidget()
         {
             Category = "Monthly Budget";
-            Dependencies = new[] { typeof(BudgetCurrencyContext), typeof(StatementModel), typeof(GlobalFilterCriteria) };
+            Dependencies = new[] { typeof(BudgetCurrencyContext), typeof(StatementModel), typeof(GlobalFilterCriteria), typeof(LedgerBook) };
             RecommendedTimeIntervalUpdate = TimeSpan.FromHours(12); // Every 12 hours.
             this.standardStyle = "WidgetStandardStyle3";
             DetailedText = "Savings Commitment";
@@ -37,6 +38,7 @@ namespace BudgetAnalyser.Engine.Widgets
             var budget = (BudgetCurrencyContext)input[0];
             var statement = (StatementModel)input[1];
             var filter = (GlobalFilterCriteria)input[2];
+            var ledger = (LedgerBook)input[3];
 
             if (statement == null || budget == null || filter == null || filter.Cleared || filter.BeginDate == null || filter.EndDate == null)
             {
@@ -48,17 +50,12 @@ namespace BudgetAnalyser.Engine.Widgets
             var totalMonths = filter.BeginDate.Value.DurationInMonths(filter.EndDate.Value);
             Maximum = Convert.ToDouble(budget.Model.Expenses.Where(s => s.Bucket is SavingsCommitmentBucket).Sum(s => s.Amount)) * totalMonths;
 
-            // Debit transactions are negative so normally the total spend will be a negative number.
-            decimal savingsToDate = -statement.Transactions.Where(t => t.BudgetBucket is SavingsCommitmentBucket && t.Amount < 0).Sum(t => t.Amount);
-            if (savingsToDate < 0)
-            {
-                savingsToDate = 0;
-            }
+            decimal savingsToDate = CalculateSavingsToDateWithTrackedLedgers(statement, ledger, filter);
 
             Value = Convert.ToDouble(savingsToDate);
             ToolTip = string.Format(CultureInfo.CurrentCulture, "You have saved {0:C} of your monthly goal {1:C}", savingsToDate, Maximum);
 
-            if ((double)savingsToDate < 0.8 * Maximum)
+            if ((double)savingsToDate < 0.9 * Maximum)
             {
                 ColourStyleName = WidgetWarningStyle;
             }
@@ -66,6 +63,50 @@ namespace BudgetAnalyser.Engine.Widgets
             {
                 ColourStyleName = this.standardStyle;
             }
+        }
+
+        private static decimal CalculateSavingsToDateWithTrackedLedgers(StatementModel statement, LedgerBook ledger, GlobalFilterCriteria filter)
+        {
+            var trackedSavingsLedgers = ledger.Ledgers
+                                            .Where(l => l.BudgetBucket is SavingsCommitmentBucket)
+                                            .Select(l => l.BudgetBucket)
+                                            .ToList();
+            if (!trackedSavingsLedgers.Any())
+            {
+                return SumDebitSavingsTransactions(statement);
+            }
+
+            decimal savingsToDate = 0;
+            foreach (var bucket in trackedSavingsLedgers)
+            {
+                var transactions = statement.Transactions.Where(t => t.BudgetBucket == bucket).ToList();
+                
+                // This will give interest earned.  This is because the transaction list will contain both debits and credits for transfering the savings around.
+                savingsToDate += transactions.Sum(t => t.Amount); 
+                
+                // This will give the savings credited.
+                var amounts = transactions
+                                .Select(t => Math.Abs(t.Amount))
+                                .GroupBy(amount => amount, amount => amount)
+                                .Where(group => group.Count() > 1);
+                savingsToDate += amounts.Distinct().Sum(g => g.Key);
+            }
+
+            // Other non-ledger-book-tracked savings will appear as debits in the statement so need to be negated.
+            var otherNontrackedSavings = statement.Transactions.Where(t => t.BudgetBucket is SavingsCommitmentBucket && trackedSavingsLedgers.All(b => b != t.BudgetBucket));
+            savingsToDate += otherNontrackedSavings.Sum(t => -t.Amount);
+            return savingsToDate;
+        }
+
+        private static decimal SumDebitSavingsTransactions(StatementModel statement)
+        {
+            var savingsToDate = -statement.Transactions.Where(t => t.BudgetBucket is SavingsCommitmentBucket && t.Amount < 0).Sum(t => t.Amount);
+            if (savingsToDate < 0)
+            {
+                savingsToDate = 0;
+            }
+
+            return savingsToDate;
         }
     }
 }
