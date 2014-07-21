f402e39a66b3a8a70336d76553b597fc9d3976f5
Benrnz <Ben@Rees.biz>
2014-07-21 22:25:20 +1200

    BudgetModel Automapper config

--- BudgetAnalyser.Engine/AutomapperConfiguration.cs
+++ BudgetAnalyser.Engine/AutomapperConfiguration.cs
@@ -29,6 +29,9 @@ namespace BudgetAnalyser.Engine
 
         public void Configure()
         {
+            // Warning! Use of this static mapping configuration has a chance of intermittently failing tests, if the tests are run in parallel (which they are in this project).
+            // This may need to change to improve consistency of test results. Failure does seem to be very rare however.
+
             Mapper.CreateMap<BudgetBucket, BudgetBucketDto>()
                 .ForMember(dto => dto.Type, m => m.MapFrom(budgetBucket => this.factory.SerialiseType(budgetBucket)));
 
