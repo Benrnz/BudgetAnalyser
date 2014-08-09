7f4f30144c8e50ca051613f50050b2e532e844a0
Benrnz <Ben@Rees.biz>
2014-08-09 21:51:14 +1200

    Increment version

--- BudgetAnalyser/Properties/AssemblyInfo.cs
+++ BudgetAnalyser/Properties/AssemblyInfo.cs
@@ -52,7 +52,7 @@ using System.Windows;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.8")]
-[assembly: AssemblyFileVersion("1.8")]
+[assembly: AssemblyVersion("1.9")]
+[assembly: AssemblyFileVersion("1.9")]
 
 [assembly: CLSCompliant(false)]
