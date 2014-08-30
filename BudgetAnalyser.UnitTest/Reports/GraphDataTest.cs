2992d0cca70f036b73b43251209750d26ab4da0a
benrnz <ben@rees.biz>
2014-08-30 15:09:19 +1200

    Some tests for GraphData

--- BudgetAnalyser.UnitTest/Reports/GraphDataTest.cs
+++ BudgetAnalyser.UnitTest/Reports/GraphDataTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using BudgetAnalyser.Engine.Reports;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace BudgetAnalyser.UnitTest.Reports
@@ -7,9 +8,84 @@ namespace BudgetAnalyser.UnitTest.Reports
     public class GraphDataTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void GraphMinimumValueShouldReturn0GivenPostiveDataBeginningWithZero()
         {
-            Assert.Inconclusive();
+            Assert.AreEqual(0, Subject.GraphMinimumValue);
+        }
+
+        [TestMethod]
+        public void GraphMinimumValueShouldReturnMinus20GivenDataWithSmallestValueOfMinus20()
+        {
+            Series3.PlotsList[2].Amount = -20;
+            Assert.AreEqual(-20, Subject.GraphMinimumValue);
+        }
+
+        [TestMethod]
+        public void VisibleShouldDefaultToTrue()
+        {
+            Assert.IsTrue(Series1.Visible);
+        }
+
+        [TestMethod]
+        public void VisibleShouldRaiseChangeEventWhenVisibleChanges()
+        {
+            bool eventRaised = false;
+            Series1.PropertyChanged += (s, e) => eventRaised = true;
+            Series1.Visible = false;
+            Assert.IsTrue(eventRaised);
+        }
+
+        private GraphData Subject { get; set; }
+
+        private SeriesData Series1 { get; set; }
+
+        private SeriesData Series2 { get; set; }
+
+        private SeriesData Series3 { get; set; }
+
+        private DateTime StartDate { get; set; }
+
+        [TestInitialize]
+        public void TestInitialise()
+        {
+            StartDate = new DateTime(2014, 08, 20);
+            Subject = new GraphData
+            {
+                GraphName = "Main Graph Title"
+            };
+
+            Series1 = new SeriesData
+                {
+                    SeriesName = "Series 1",
+                    Description = "Series 1 Description",
+                };
+
+            Series2 = new SeriesData
+            {
+                SeriesName = "Series 2",
+                Description = "Series 2 Description",
+            };
+
+            Series3 = new SeriesData
+            {
+                SeriesName = "Series 3",
+                Description = "Series 3 Description",
+            };
+            
+            Subject.SeriesList.Add(Series1);
+            Subject.SeriesList.Add(Series2);
+            Subject.SeriesList.Add(Series3);
+
+            int seriesNumber = 0;
+            foreach (var series in Subject.SeriesList)
+            {
+                for (int index = 0; index < 31; index++)
+                {
+                    series.PlotsList.Add(new DatedGraphPlot { Amount = seriesNumber * 100 * (index + 1), Date = StartDate.AddDays(index) });
+                }
+
+                seriesNumber++;
+            }
         }
     }
 }
