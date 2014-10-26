62948eea2397a95c1d03e1f2499dd3ab544b9eb2
benrnz <ben@rees.biz>
2014-10-26 15:12:18 +1300

    Increment total number of tests test

--- BudgetAnalyser.UnitTest/MetaTest.cs
+++ BudgetAnalyser.UnitTest/MetaTest.cs
@@ -29,7 +29,7 @@ namespace BudgetAnalyser.UnitTest
             }
         }
 
-        private const int ExpectedMinimumTests = 680;
+        private const int ExpectedMinimumTests = 686;
 
         [TestMethod]
         public void NoDecreaseInTests()
