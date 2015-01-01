f4e2f6a438d303f8811b3f5cff6a165186915856
benrnz <ben@rees.biz>
2015-01-02 12:45:56 +1300

    Updated test count

--- BudgetAnalyser.UnitTest/MetaTest.cs
+++ BudgetAnalyser.UnitTest/MetaTest.cs
@@ -29,7 +29,7 @@ namespace BudgetAnalyser.UnitTest
             }
         }
 
-        private const int ExpectedMinimumTests = 698;
+        private const int ExpectedMinimumTests = 731;
 
         [TestMethod]
         public void NoDecreaseInTests()
