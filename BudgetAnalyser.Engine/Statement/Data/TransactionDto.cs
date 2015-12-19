705becff0b8bf28befb1a6eb2c8d00de77f7f553
benrnz <ben@rees.biz>
2015-12-19 20:20:29 +1300

    Added Statement Dto types

--- BudgetAnalyser.Engine/Statement/Data/TransactionDto.cs
+++ BudgetAnalyser.Engine/Statement/Data/TransactionDto.cs
@@ -2,12 +2,30 @@
 
 namespace BudgetAnalyser.Engine.Statement.Data
 {
+    /// <summary>
+    ///     A Dto to persist a single transaction from a statement.
+    /// </summary>
     public class TransactionDto
     {
+        /// <summary>
+        /// Gets or sets the account code.
+        /// </summary>
         public string Account { get; set; }
+        /// <summary>
+        /// Gets or sets the transaction amount, debits are negative.
+        /// </summary>
         public decimal Amount { get; set; }
+        /// <summary>
+        /// Gets or sets the budget bucket code.
+        /// </summary>
         public string BudgetBucketCode { get; set; }
+        /// <summary>
+        /// Gets or sets the transaction date.
+        /// </summary>
         public DateTime Date { get; set; }
+        /// <summary>
+        /// Gets or sets the transaction description.
+        /// </summary>
         public string Description { get; set; }
 
         /// <summary>
@@ -16,9 +34,21 @@ namespace BudgetAnalyser.Engine.Statement.Data
         /// </summary>
         public Guid Id { get; set; }
 
+        /// <summary>
+        /// Gets or sets the transaction reference1.
+        /// </summary>
         public string Reference1 { get; set; }
+        /// <summary>
+        /// Gets or sets the transaction reference2.
+        /// </summary>
         public string Reference2 { get; set; }
+        /// <summary>
+        /// Gets or sets the transaction reference3.
+        /// </summary>
         public string Reference3 { get; set; }
+        /// <summary>
+        /// Gets or sets the type code of the transaction.
+        /// </summary>
         public string TransactionType { get; set; }
     }
 }
