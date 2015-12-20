689ebf93355078137cafc838b316178e97ba3756
benrnz <ben@rees.biz>
2015-12-20 14:47:20 +1300

    Added todos still outsanding

---
+++ BudgetAnalyser.Engine/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BudgetAnalyser.Engine
+{
+    // TODO Remove automapper and use manual mapping for simple low cost low complexity mapping
+    // TODO Re-wire IoC with Autofac PCL
+    // TODO Redesign application state persistence so it doesnt depend on Rees.UserInteraction.Contracts.
+}
