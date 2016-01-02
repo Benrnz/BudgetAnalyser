ed150c968690aaa3a9ec33a1e845f0d729123ee6
benrnz <ben@rees.biz>
2016-01-02 13:28:43 +1300

    Automatically copy generated code onto the clipboard

--- MapperGeneration/MapperGenerator.cs
+++ MapperGeneration/MapperGenerator.cs
@@ -1,4 +1,6 @@
 using System.Text;
+using System.Threading;
+using System.Windows;
 using Rees.TangyFruitMapper;
 using Xunit.Abstractions;
 
@@ -6,26 +8,34 @@ namespace MapperGeneration
 {
     public abstract class MapperGenerator
     {
-        private readonly ITestOutputHelper output;
         private readonly MappingGenerator codeGenerator;
         private readonly StringBuilder diagnosticLogger;
+        private readonly StringBuilder generatedCode;
+        private readonly ITestOutputHelper output;
 
         protected MapperGenerator(ITestOutputHelper output)
         {
             this.output = output;
+            this.generatedCode = new StringBuilder();
             this.diagnosticLogger = new StringBuilder();
             this.codeGenerator = new MappingGenerator
             {
                 DiagnosticLogging = l => this.diagnosticLogger.AppendLine(l),
-                EmitWithInternalAccessors = true,
+                EmitWithInternalAccessors = true
             };
         }
+
+        protected string GeneratedCode => this.generatedCode.ToString();
 
         protected void Act<TDto, TModel>()
         {
             try
             {
-                this.codeGenerator.Generate<TDto, TModel>(this.output.WriteLine);
+                this.codeGenerator.Generate<TDto, TModel>(codeLine =>
+                {
+                    this.generatedCode.AppendLine(codeLine);
+                    this.output.WriteLine(codeLine);
+                });
             }
             finally
             {
@@ -33,6 +43,11 @@ namespace MapperGeneration
                 this.output.WriteLine("===========================================================================");
                 this.output.WriteLine(this.diagnosticLogger.ToString());
             }
+
+            var staThread = new Thread(() => Clipboard.SetText(GeneratedCode));
+            staThread.SetApartmentState(ApartmentState.STA);
+            staThread.Start();
+            staThread.Join();
         }
     }
 }
