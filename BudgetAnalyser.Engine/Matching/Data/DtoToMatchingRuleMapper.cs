936ce97c5ecbdfd2eae84750cb33f06331807689
benrnz <ben@rees.biz>
2016-01-02 15:18:13 +1300

    Added matching rules mapper

--- BudgetAnalyser.Engine/Matching/Data/DtoToMatchingRuleMapper.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace BudgetAnalyser.Engine.Matching.Data
-{
-    [AutoRegisterWithIoC(SingleInstance = true, RegisterAs = typeof(BasicMapper<MatchingRuleDto, MatchingRule>))]
-    public class DtoToMatchingRuleMapper : MagicMapper<MatchingRuleDto, MatchingRule>
-    {
-    }
-}
--- BudgetAnalyser.Engine/Matching/Data/MatchingRuleDomainToDataMapper.cs
+++ BudgetAnalyser.Engine/Matching/Data/MatchingRuleDomainToDataMapper.cs
@@ -1,7 +1,33 @@
-﻿namespace BudgetAnalyser.Engine.Matching.Data
+﻿using System;
+using BudgetAnalyser.Engine.Budget;
+using JetBrains.Annotations;
+
+namespace BudgetAnalyser.Engine.Matching.Data
 {
-    [AutoRegisterWithIoC(SingleInstance = true, RegisterAs = typeof(BasicMapper<MatchingRule, MatchingRuleDto>))]
-    public class MatchingRuleDomainToDataMapper : MagicMapper<MatchingRule, MatchingRuleDto>
+    internal partial class Mapper_MatchingRuleDto_MatchingRule
     {
+        private readonly IBudgetBucketRepository bucketRepo;
+
+        public Mapper_MatchingRuleDto_MatchingRule([NotNull] IBudgetBucketRepository bucketRepo)
+        {
+            if (bucketRepo == null) throw new ArgumentNullException(nameof(bucketRepo));
+            this.bucketRepo = bucketRepo;
+        }
+
+        // ReSharper disable once RedundantAssignment
+        partial void ModelFactory(MatchingRuleDto dto, ref MatchingRule model)
+        {
+            model = new MatchingRule(this.bucketRepo);
+        }
+
+        partial void ToModelPostprocessing(MatchingRuleDto dto, ref MatchingRule model)
+        {
+            model.BucketCode = dto.BucketCode;
+        }
+
+        partial void ToDtoPostprocessing(ref MatchingRuleDto dto, MatchingRule model)
+        {
+            dto.BucketCode = model.Bucket.Code;
+        }
     }
 }
