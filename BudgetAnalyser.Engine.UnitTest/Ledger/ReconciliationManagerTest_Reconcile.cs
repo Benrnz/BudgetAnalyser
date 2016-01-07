ffb046ada38f2eec275415226e636761109d2218
benrnz <ben@rees.biz>
2016-01-07 16:15:42 +1300

    All ledger tests now added and passing

--- BudgetAnalyser.Engine.UnitTest/Ledger/ReconciliationManagerTest_Reconcile.cs
+++ BudgetAnalyser.Engine.UnitTest/Ledger/ReconciliationManagerTest_Reconcile.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using BudgetAnalyser.Engine;
 using BudgetAnalyser.Engine.Budget;
 using BudgetAnalyser.Engine.Ledger;
 using BudgetAnalyser.Engine.Matching;
