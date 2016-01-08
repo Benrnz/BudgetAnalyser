f848a118774ca5328e5d0fcac56f6adcdd317de0
benrnz <ben@rees.biz>
2016-01-09 11:24:19 +1300

    Added a default logger to log to Debug stream

---
+++ BudgetAnalyser.Engine/DebugLogger.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace BudgetAnalyser.Engine
+{
+    /// <summary>
+    ///     A logging implementation that simply outputs to the Debug stream <see cref="System.Diagnostics.Debug" />.
+    /// </summary>
+    [AutoRegisterWithIoC]
+    public class DebugLogger : ILogger
+    {
+        /// <summary>
+        ///     A custom string format method to avoid code translation and localisation warnings. The logging language is always
+        ///     English.
+        /// </summary>
+        public string Format(string formatTemplate, params object[] parameters)
+        {
+            return string.Format(formatTemplate, parameters);
+        }
+
+        /// <summary>
+        ///     Write a debug/diagnostic log entry regardless of the configured log level.
+        /// </summary>
+        public void LogAlways(Func<ILogger, string> logEntryBuilder)
+        {
+            Debug.WriteLine(ConstructLogEntry("ALWAYS", logEntryBuilder));
+        }
+
+        /// <summary>
+        ///     Write a debug/diagnostic log entry if the configured log level is set to error.
+        /// </summary>
+        public void LogError(Func<ILogger, string> logEntryBuilder)
+        {
+            Debug.WriteLine(ConstructLogEntry("ERROR", logEntryBuilder));
+        }
+
+        /// <summary>
+        ///     Write a debug/diagnostic log entry if the configured log level is set to error.
+        /// </summary>
+        public void LogError(Exception ex, Func<ILogger, string> logEntryBuilder)
+        {
+            Debug.WriteLine(ConstructLogEntry("ERROR", logEntryBuilder));
+        }
+
+        /// <summary>
+        ///     Write a debug/diagnostic log entry if the configured log level is set to info.
+        /// </summary>
+        public void LogInfo(Func<ILogger, string> logEntryBuilder)
+        {
+            Debug.WriteLine("INFO   " + logEntryBuilder(this));
+        }
+
+        /// <summary>
+        ///     Write a debug/diagnostic log entry if the configured log level is set to warning.
+        /// </summary>
+        public void LogWarning(Func<ILogger, string> logEntryBuilder)
+        {
+            Debug.WriteLine(ConstructLogEntry("WARN", logEntryBuilder));
+        }
+
+        private string ConstructLogEntry(string level, Func<ILogger, string> logEntryBuilder)
+        {
+            return DateTime.Now.ToString("yy-MM-dThh:mm:ss.fff") + level.PadRight(7) + logEntryBuilder(this);
+        }
+    }
+}
