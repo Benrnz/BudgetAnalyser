6edd718dfc96783f31226623ef6539714c0c6d47
Ben <Ben@Rees.biz>
2016-04-23 14:00:06 +1200

    Xamarin basic infrastructure (#112)
    * Added Android project and began config of it
    
    * App changes for Xamarin start up
    
    * Remove Project.lock.json file from source control
    
    * Better structure, and updated packages
    
    * Mobile projects running and tidy

--- BudgetAnalyser.Mobile/BudgetAnalyser.Mobile.UWP/Properties/AssemblyInfo.cs
+++ BudgetAnalyser.Mobile/BudgetAnalyser.Mobile.UWP/Properties/AssemblyInfo.cs
@@ -1,28 +1,18 @@
 ﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-
-[assembly: AssemblyTitle("Rees.TestUtilities")]
-[assembly: AssemblyDescription("A group of utilities to assist in unit testing.")]
+[assembly: AssemblyTitle("BudgetAnalyser.Mobile.UWP")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Rees.biz")]
-[assembly: AssemblyProduct("Rees.TestUtilities")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BudgetAnalyser.Mobile.UWP")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-
-[assembly: Guid("5d0f215c-f88e-4e70-955a-d6cb6a353d5e")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -34,6 +24,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: ComVisible(false)]
