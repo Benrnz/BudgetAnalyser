eecb3eef2b66907b96fe057a1867e1ec283fb4af
Ben Rees <ben@rees.biz>
2024-08-16 18:55:59 +1200

    Fix widgets not being clickable

--- BudgetAnalyser/Dashboard/DashboardController.cs
+++ BudgetAnalyser/Dashboard/DashboardController.cs
@@ -96,10 +96,6 @@ namespace BudgetAnalyser.Dashboard
             }
         }
 
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance", "CA1822:MarkMembersAsStatic", Justification = "Used by data binding")]
-        [UsedImplicitly]
-        public ICommand WidgetActivatedCommand => WidgetCommands.WidgetClickedCommand;
-
         public ObservableCollection<WidgetGroup> WidgetGroups { get; private set; }
 
         private void OnApplicationStateLoadedMessageReceived([NotNull] ApplicationStateLoadedMessage message)
