d224f64289337ef66d5d41315921506f4e015f39
Ben Rees <ben@rees.biz>
2024-08-27 20:30:36 +1200

    Fix overall perf report

--- BudgetAnalyser/ReportsCatalog/ReportsCatalogController.cs
+++ BudgetAnalyser/ReportsCatalog/ReportsCatalogController.cs
@@ -1,107 +1,82 @@
-﻿using System.Windows.Input;
-using BudgetAnalyser.Budget;
+﻿using BudgetAnalyser.Budget;
 using BudgetAnalyser.Engine;
 using BudgetAnalyser.Engine.Budget;
 using BudgetAnalyser.Engine.Statement;
 using BudgetAnalyser.Filtering;
-using BudgetAnalyser.LedgerBook;
 using BudgetAnalyser.ReportsCatalog.OverallPerformance;
 using BudgetAnalyser.Statement;
-using CommunityToolkit.Mvvm.Input;
 using CommunityToolkit.Mvvm.Messaging;
 using Rees.Wpf;
 
-namespace BudgetAnalyser.ReportsCatalog
+namespace BudgetAnalyser.ReportsCatalog;
+
+[AutoRegisterWithIoC(SingleInstance = true)]
+public class ReportsCatalogController : ControllerBase, IShowableController
 {
-    [AutoRegisterWithIoC(SingleInstance = true)]
-    public class ReportsCatalogController : ControllerBase, IShowableController
+    private readonly NewWindowViewLoader newWindowViewLoader;
+    private BudgetCollection budgets;
+    private StatementModel? currentStatementModel;
+    private bool doNotUseShown;
+
+    public ReportsCatalogController([NotNull] UiContext uiContext, [NotNull] NewWindowViewLoader newWindowViewLoader) : base(uiContext.Messenger)
     {
-        private readonly NewWindowViewLoader newWindowViewLoader;
-        private BudgetCollection budgets;
-        private Engine.Ledger.LedgerBook currentLedgerBook;
-        private StatementModel currentStatementModel;
-        private bool doNotUseShown;
-
-        public ReportsCatalogController([NotNull] UiContext uiContext, [NotNull] NewWindowViewLoader newWindowViewLoader) : base(uiContext.Messenger)
+        if (uiContext == null)
         {
-            if (uiContext == null)
-            {
-                throw new ArgumentNullException(nameof(uiContext));
-            }
-
-            if (newWindowViewLoader == null)
-            {
-                throw new ArgumentNullException(nameof(newWindowViewLoader));
-            }
-
-            this.newWindowViewLoader = newWindowViewLoader;
-            OverallPerformanceController = uiContext.OverallPerformanceController;
-
-            Messenger.Register<ReportsCatalogController, StatementReadyMessage>(this, static (r, m) => r.OnStatementReadyMessageReceived(m));
-            Messenger.Register<ReportsCatalogController, BudgetReadyMessage>(this, static (r, m) => r.OnBudgetReadyMessageReceived(m));
-            Messenger.Register<ReportsCatalogController, LedgerBookReadyMessage>(this, static (r, m) => r.OnLedgerBookReadyMessageReceived(m));
+            throw new ArgumentNullException(nameof(uiContext));
         }
 
-        [UsedImplicitly]
-        public ICommand OverallBudgetPerformanceCommand => new RelayCommand(OnOverallBudgetPerformanceCommandExecute, CanExecuteOverallBudgetPerformanceCommand);
+        this.newWindowViewLoader = newWindowViewLoader ?? throw new ArgumentNullException(nameof(newWindowViewLoader));
+        OverallPerformanceController = uiContext.OverallPerformanceController;
 
-        public OverallPerformanceController OverallPerformanceController { get; }
+        Messenger.Register<ReportsCatalogController, StatementReadyMessage>(this, static (r, m) => r.OnStatementReadyMessageReceived(m));
+        Messenger.Register<ReportsCatalogController, BudgetReadyMessage>(this, static (r, m) => r.OnBudgetReadyMessageReceived(m));
+    }
 
-        public bool Shown
+    public OverallPerformanceController OverallPerformanceController { get; }
+
+    public bool OverallPerformanceReportIsAvailable =>
+        this.currentStatementModel != null
+        && this.currentStatementModel.Transactions.Any()
+        && this.budgets?.CurrentActiveBudget != null;
+
+    public bool Shown
+    {
+        get => this.doNotUseShown;
+        set
         {
-            get { return this.doNotUseShown; }
-            set
-            {
-                if (value == this.doNotUseShown)
-                {
-                    return;
-                }
-                this.doNotUseShown = value;
-                OnPropertyChanged();
-            }
+            if (value == this.doNotUseShown) return;
+            this.doNotUseShown = value;
+            OnPropertyChanged();
         }
+    }
 
-        private bool CanExecuteOverallBudgetPerformanceCommand()
-        {
-            return this.currentStatementModel != null
-                   && this.currentStatementModel.Transactions.Any()
-                   && this.budgets?.CurrentActiveBudget != null;
-        }
+    public void ShowOverallPerformanceReport()
+    {
+        if (this.currentStatementModel == null) return;
+        OverallPerformanceController.Load(this.currentStatementModel, this.budgets, RequestCurrentFilter());
 
-        private void OnBudgetReadyMessageReceived(BudgetReadyMessage message)
-        {
-            this.budgets = message.Budgets;
-        }
+        this.newWindowViewLoader.MinHeight = this.newWindowViewLoader.Height = 650;
+        this.newWindowViewLoader.MinWidth = this.newWindowViewLoader.Width = 740;
+        this.newWindowViewLoader.Show(OverallPerformanceController);
+    }
 
-        private void OnLedgerBookReadyMessageReceived([NotNull] LedgerBookReadyMessage message)
-        {
-            if (message == null)
-            {
-                throw new ArgumentNullException(nameof(message));
-            }
+    private void OnBudgetReadyMessageReceived(BudgetReadyMessage message)
+    {
+        this.budgets = message.Budgets;
+        OnPropertyChanged(nameof(OverallPerformanceReportIsAvailable));
+    }
 
-            this.currentLedgerBook = message.LedgerBook;
-        }
+    private void OnStatementReadyMessageReceived(StatementReadyMessage message)
+    {
+        this.currentStatementModel = message.StatementModel;
+        OnPropertyChanged(nameof(OverallPerformanceReportIsAvailable));
+    }
 
-        private void OnOverallBudgetPerformanceCommandExecute()
-        {
-            OverallPerformanceController.Load(this.currentStatementModel, this.budgets, RequestCurrentFilter());
-
-            this.newWindowViewLoader.MinHeight = this.newWindowViewLoader.Height = 650;
-            this.newWindowViewLoader.MinWidth = this.newWindowViewLoader.Width = 740;
-            this.newWindowViewLoader.Show(OverallPerformanceController);
-        }
-
-        private void OnStatementReadyMessageReceived(StatementReadyMessage message)
-        {
-            this.currentStatementModel = message.StatementModel;
-        }
-
-        private GlobalFilterCriteria RequestCurrentFilter()
-        {
-            var currentFilterMessage = new RequestFilterMessage(this);
-            Messenger.Send(currentFilterMessage);
-            return currentFilterMessage.Criteria;
-        }
+    private GlobalFilterCriteria RequestCurrentFilter()
+    {
+        // TODO it would be better if reporting operated on its own independent date range to the global filter.
+        var currentFilterMessage = new RequestFilterMessage(this);
+        Messenger.Send(currentFilterMessage);
+        return currentFilterMessage.Criteria;
     }
 }
