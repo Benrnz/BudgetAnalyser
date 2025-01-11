8def0be20de0d0b90c23b7f23ac4e2855934e6a3
Ben Rees <ben@rees.biz>
2025-01-11 23:09:58 +1300

    Resolve and rethink todo comment.

--- BudgetAnalyser.Engine/Ledger/Reconciliation/ReconciliationConsistency.cs
+++ BudgetAnalyser.Engine/Ledger/Reconciliation/ReconciliationConsistency.cs
@@ -1,9 +1,13 @@
 ﻿namespace BudgetAnalyser.Engine.Ledger.Reconciliation;
 
+/// <summary>
+///     This class is used as a factory to create a <see cref="IDisposable"/> instance used to ensure the <see cref="LedgerBook"/> is left in a valid and consistent state after a reconciliation.
+///     Even if the reconciliation fails and throws an exception, this mechanism ensures clean up code is run, effectively rolling back. As a factory class this gives a good unit testing
+///     mechanism to exploit also.
+/// </summary>
 [AutoRegisterWithIoC]
 internal class ReconciliationConsistency : IReconciliationConsistency
 {
-    // TODO refactor this. Its silly to have this passing thru to another class.
     public IDisposable EnsureConsistency(LedgerBook book)
     {
         return new ReconciliationConsistencyChecker(book);
