6e8a99ea39fb34bc8566795b15112f136b78e53c
Ben Rees <ben@rees.biz>
2025-01-11 16:32:08 +1300

    Removed ID from LedgerBucket

--- BudgetAnalyser.Engine/Ledger/Data/LedgerBucketDto.cs
+++ BudgetAnalyser.Engine/Ledger/Data/LedgerBucketDto.cs
@@ -6,22 +6,9 @@
 public class LedgerBucketDto
 {
     /// <summary>
-    ///     Instantiate a new instance of the <see cref="LedgerBucketDto" /> class.
-    /// </summary>
-    public LedgerBucketDto()
-    {
-        Id = Guid.NewGuid();
-    }
-
-    /// <summary>
     ///     Gets or sets the bucket code.
     /// </summary>
     public required string BucketCode { get; init; }
-
-    /// <summary>
-    ///     A unique token to identify this ledger bucket when using the code is not appropriate.
-    /// </summary>
-    public Guid Id { get; init; }
 
     /// <summary>
     ///     Gets or sets the bank account that this ledger bucket is stored in.
