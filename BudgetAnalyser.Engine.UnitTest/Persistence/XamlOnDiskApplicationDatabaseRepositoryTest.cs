d0b20f7c39d4a090a2d72c1cd45e41dbe6201545
Ben Rees <ben@rees.biz>
2025-01-19 00:08:17 +1300

    Added new mapper for ApplicationDatabase.

--- BudgetAnalyser.Engine.UnitTest/Persistence/XamlOnDiskApplicationDatabaseRepositoryTest.cs
+++ BudgetAnalyser.Engine.UnitTest/Persistence/XamlOnDiskApplicationDatabaseRepositoryTest.cs
@@ -1,58 +1,52 @@
-﻿using System.Threading.Tasks;
-using BudgetAnalyser.Engine.Persistence;
+﻿using BudgetAnalyser.Engine.Persistence;
 using BudgetAnalyser.Engine.UnitTest.TestData;
 using BudgetAnalyser.Engine.UnitTest.TestHarness;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace BudgetAnalyser.Engine.UnitTest.Persistence
+namespace BudgetAnalyser.Engine.UnitTest.Persistence;
+
+[TestClass]
+public class XamlOnDiskApplicationDatabaseRepositoryTest
 {
-    [TestClass]
-    public class XamlOnDiskApplicationDatabaseRepositoryTest
+    private ApplicationDatabase result;
+    private XamlOnDiskApplicationDatabaseRepositoryTestHarness subject;
+
+    [TestMethod]
+    public void LoadShouldSetBudgetCollectionStorageKeyGivenDemoFile()
     {
-        private ApplicationDatabase result;
-        private XamlOnDiskApplicationDatabaseRepositoryTestHarness subject;
+        Assert.AreEqual("DemoBudget.xml", this.result.BudgetCollectionStorageKey);
+    }
 
-        [TestMethod]
-        public void LoadShouldSetBudgetCollectionStorageKeyGivenDemoFile()
-        {
-            Assert.AreEqual("DemoBudget.xml", this.result.BudgetCollectionStorageKey);
-        }
+    [TestMethod]
+    public void LoadShouldSetLedgerBookStorageKeyGivenDemoFile()
+    {
+        Assert.AreEqual("DemoLedgerBook.xml", this.result.LedgerBookStorageKey);
+    }
 
-        [TestMethod]
-        public void LoadShouldSetLedgerBookStorageKeyGivenDemoFile()
-        {
-            Assert.AreEqual("DemoLedgerBook.xml", this.result.LedgerBookStorageKey);
-        }
+    [TestMethod]
+    public void LoadShouldSetMatchingRulesStorageKeyGivenDemoFile()
+    {
+        Assert.AreEqual("DemoMatchingRules.xml", this.result.MatchingRulesCollectionStorageKey);
+    }
 
-        [TestMethod]
-        public void LoadShouldSetMatchingRulesStorageKeyGivenDemoFile()
-        {
-            Assert.AreEqual("DemoMatchingRules.xml", this.result.MatchingRulesCollectionStorageKey);
-        }
+    [TestMethod]
+    public void LoadShouldSetReconciliationTasksGivenDemoFile()
+    {
+        Assert.AreEqual(2, this.result.LedgerReconciliationToDoCollection.Count);
+    }
 
-        [TestMethod]
-        public void LoadShouldSetReconciliationTasksGivenDemoFile()
-        {
-            Assert.AreEqual(2, this.result.LedgerReconciliationToDoCollection.Count);
-        }
+    [TestMethod]
+    public void LoadShouldSetStatementModelStorageKeyGivenDemoFile()
+    {
+        Assert.AreEqual("DemoTransactions.csv", this.result.StatementModelStorageKey);
+    }
 
-        [TestMethod]
-        public void LoadShouldSetStatementModelStorageKeyGivenDemoFile()
-        {
-            Assert.AreEqual("DemoTransactions.csv", this.result.StatementModelStorageKey);
-        }
+    [TestInitialize]
+    public void TestInitialise()
+    {
+        this.subject = new XamlOnDiskApplicationDatabaseRepositoryTestHarness(new MapperApplicationDatabaseToStorageRoot2()) { FileExistsOverride = fileName => true };
 
-        [TestInitialize]
-        public void TestInitialise()
-        {
-            this.subject = new XamlOnDiskApplicationDatabaseRepositoryTestHarness(new MapperBudgetAnalyserStorageRoot2ApplicationDatabase())
-            {
-                FileExistsOverride = fileName => true
-            };
-
-            var task = this.subject.LoadAsync(TestDataConstants.DemoBudgetAnalyserFileName);
-            task.Wait();
-            this.result = task.Result;
-        }
+        var task = this.subject.LoadAsync(TestDataConstants.DemoBudgetAnalyserFileName);
+        task.Wait();
+        this.result = task.Result;
     }
 }
---
+++ BudgetAnalyser.Engine/Persistence/MapperApplicationDatabaseToStorageRoot2.cs
@@ -0,0 +1,39 @@
+﻿using BudgetAnalyser.Engine.Ledger;
+using BudgetAnalyser.Engine.Ledger.Data;
+using Rees.TangyFruitMapper;
+
+namespace BudgetAnalyser.Engine.Persistence;
+
+[AutoRegisterWithIoC]
+public class MapperApplicationDatabaseToStorageRoot2 : IDtoMapper<BudgetAnalyserStorageRoot, ApplicationDatabase>
+{
+    private readonly IDtoMapper<List<ToDoTaskDto>, ToDoCollection> todoMapper = new MapperToDoCollectionToDto2();
+
+    public BudgetAnalyserStorageRoot ToDto(ApplicationDatabase model)
+    {
+        var dto = new BudgetAnalyserStorageRoot
+        {
+            BudgetCollectionRootDto = new StorageBranch { Source = model.BudgetCollectionStorageKey },
+            LedgerBookRootDto = new StorageBranch { Source = model.LedgerBookStorageKey },
+            StatementModelRootDto = new StorageBranch { Source = model.StatementModelStorageKey },
+            LedgerReconciliationToDoCollection = this.todoMapper.ToDto(model.LedgerReconciliationToDoCollection),
+            MatchingRulesCollectionRootDto = new StorageBranch { Source = model.MatchingRulesCollectionStorageKey },
+            IsEncrypted = model.IsEncrypted
+        };
+        return dto;
+    }
+
+    public ApplicationDatabase ToModel(BudgetAnalyserStorageRoot dto)
+    {
+        var baxModel = new ApplicationDatabase
+        {
+            BudgetCollectionStorageKey = dto.BudgetCollectionRootDto.Source,
+            LedgerBookStorageKey = dto.LedgerBookRootDto.Source,
+            StatementModelStorageKey = dto.StatementModelRootDto.Source,
+            MatchingRulesCollectionStorageKey = dto.MatchingRulesCollectionRootDto.Source,
+            IsEncrypted = dto.IsEncrypted,
+            LedgerReconciliationToDoCollection = this.todoMapper.ToModel(dto.LedgerReconciliationToDoCollection)
+        };
+        return baxModel;
+    }
+}
