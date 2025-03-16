5b6123a934bc57f43e2364a4ea8951aca4674b2d
Ben Rees <ben@rees.biz>
2025-03-16 18:08:00 +1300

    Ensure All WPF views that use DatePicker have a converter to use DateOnly.

---
+++ BudgetAnalyser/Converters/DateOnlyToDateTimeConverter.cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace BudgetAnalyser.Converters;
+
+public class DateOnlyToDateTimeConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is DateOnly dateOnly)
+        {
+            return dateOnly.ToDateTime(TimeOnly.MinValue);
+        }
+
+        return null;
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is DateTime dateTime)
+        {
+            return DateOnly.FromDateTime(dateTime);
+        }
+
+        return null;
+    }
+}
