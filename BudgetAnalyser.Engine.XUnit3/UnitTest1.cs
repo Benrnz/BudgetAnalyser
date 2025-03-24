d02fb7153a88f863220f75c367f597dba84c6876
Ben <Ben@Rees.biz>
2025-03-24 21:49:36 +1300

    Add in new empty xunit3 project (#189)
    * Add in new empty xunit3 project
    
    * Rename test. Put onhold XUnit3 migration until Rider can run the tests. Tests pass in pipeline and commandline only.

---
+++ BudgetAnalyser.Engine.XUnit3/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace BudgetAnalyser.Engine.XUnit3;
+
+public class UnitTest1
+{
+    [Fact]
+    public void RiderRunnerTest()
+    {
+        // Migrate tests once this passes in Rider's runner
+        Assert.True(true);
+    }
+}
