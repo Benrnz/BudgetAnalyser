4620f5faa95025a09d32dd9e78351a8bce006cd8
Ben Rees <ben@rees.biz>
2025-04-04 21:17:21 +1300

    Copy files into new project

--- BudgetAnalyser.Engine.XUnit/Helpers/IReesTestOutput.cs
+++
@@ -1,9 +0,0 @@
-namespace BudgetAnalyser.Engine.XUnit.Helpers;
-
-public interface IReesTestOutput
-{
-    void Write(string text);
-    void Write(string template, params object[] args);
-    void WriteLine(string line);
-    void WriteLine(string template, params object[] args);
-}
