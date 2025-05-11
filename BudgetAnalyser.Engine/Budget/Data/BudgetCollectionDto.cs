135c3df4ed7d31cac750674ddfe7764740212df5
Ben Rees <ben@rees.biz>
2025-05-11 14:47:07 +1200

    Change Budget collection dto to a record

--- BudgetAnalyser.Engine/Budget/Data/BudgetCollectionDto.cs
+++ BudgetAnalyser.Engine/Budget/Data/BudgetCollectionDto.cs
@@ -1,35 +1,22 @@
-﻿using System.Diagnostics.CodeAnalysis;
-using JetBrains.Annotations;
-
-namespace BudgetAnalyser.Engine.Budget.Data;
+﻿namespace BudgetAnalyser.Engine.Budget.Data;
 
 /// <summary>
 ///     A Dto object to persist a collection of budgets.
 /// </summary>
-[SuppressMessage("Microsoft.Naming", "CA1711:IdentifiersShouldNotHaveIncorrectSuffix", Justification = "Necessary for convention")]
-public class BudgetCollectionDto
+public record BudgetCollectionDto
 {
     /// <summary>
-    ///     Gets or sets the buckets included in the buget collection.
+    ///     Gets or sets the buckets included in the budget collection.
     /// </summary>
-    [SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly", Justification = "Necessary for serialisation")]
-    [SuppressMessage("Microsoft.Design", "CA1002:DoNotExposeGenericLists", Justification = "Necessary for serialisation")]
-    public List<BudgetBucketDto> Buckets
-    {
-        get;
-        [UsedImplicitly]
-        set;
-    } = new();
+    public List<BudgetBucketDto> Buckets { get; init; } = new();
 
     /// <summary>
     ///     Gets or sets the budgets included in the budget collection.
     /// </summary>
-    [SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly", Justification = "Necessary for serialisation")]
-    [SuppressMessage("Microsoft.Design", "CA1002:DoNotExposeGenericLists", Justification = "Necessary for serialisation")]
-    public List<BudgetModelDto> Budgets { get; set; } = new();
+    public List<BudgetModelDto> Budgets { get; init; } = new();
 
     /// <summary>
     ///     Gets or sets the storage key.
     /// </summary>
-    public string StorageKey { get; set; } = string.Empty;
+    public string StorageKey { get; init; } = string.Empty;
 }
