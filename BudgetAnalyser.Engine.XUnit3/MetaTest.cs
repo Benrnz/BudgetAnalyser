1b02bb508c897c422f9df49be8dece4ab49fe2c3
Ben Rees <ben@rees.biz>
2026-07-01 13:10:23 +1200

    Remove old Unit Test Project

--- BudgetAnalyser.Engine.XUnit3/MetaTest.cs
+++ BudgetAnalyser.Engine.XUnit3/MetaTest.cs
@@ -5,7 +5,7 @@ namespace BudgetAnalyser.Engine.XUnit;
 
 public class MetaTest(ITestOutputHelper testOutputHelper)
 {
-    private const int MinimumTestCount = 291;
+    private const int MinimumTestCount = 1062;
 
     [Fact]
     public void ListAllTests()
